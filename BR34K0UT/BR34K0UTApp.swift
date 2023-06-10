4cf1d7c377ef2de4b55392db40723f941f8facf7
TOMOQ1024 <tomo_suzu_16384@au.com>
2023-06-11 01:53:43 +0900

    Initial Commit

---
+++ BR34K0UT/BR34K0UTApp.swift
@@ -0,0 +1,18 @@
+//
+//  BR34K0UTApp.swift
+//  BR34K0UT
+//  
+//  Created by tomoq on 2023/06/11
+//  
+//
+
+import SwiftUI
+
+@main
+struct BR34K0UTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BR34K0UT/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  BR34K0UT
+//  
+//  Created by tomoq on 2023/06/11
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
